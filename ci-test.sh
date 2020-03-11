d3ca018a123652dfbaea9d1980d3e44c2483c2ea
Jeremy Busk <jeremybusk@gmail.com>
2020-03-11 07:52:55 +0000

    add sudo to ls

--- ci-test.sh
+++ ci-test.sh
@@ -5,4 +5,4 @@ echo "test 1 file" > test1.txt
 echo "test 2 file" > test2.txt
 sftpuseradd --sftp-user jtest1 --sftp-pass jtest1
 ./sftp-test.py -u jtest1 -p jtest1
-ls -laht /sftp/*
+sudo ls -laht /sftp/*
