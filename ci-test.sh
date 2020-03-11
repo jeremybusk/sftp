1fb915744cd0a16e9bef6556ede4c69049583488
Jeremy Busk <jeremybusk@gmail.com>
2020-03-11 08:36:47 +0000

    modify nftables

--- ci-test.sh
+++ ci-test.sh
@@ -6,4 +6,4 @@ echo "test 2 file" > test2.txt
 sftpuseradd --sftp-user jtest1 --sftp-pass jtest1
 ./sftp-test.py -u jtest1 -p jtest1
 # sudo ls -laht /sftp/*
-sudo ls -laht /*
+sudo ls -laht /sftp/jtest1/*
