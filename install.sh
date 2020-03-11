3418fbfa8a033b9e2487f9cdac01994e7b7d419d
Jeremy Busk <jeremybusk@gmail.com>
2020-03-11 08:18:39 +0000

    add nftables

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -exo pipefail
 
 sudo apt-get update
-sudo apt-get install -y openssh-server python3 shellcheck
+sudo apt-get install -y nftables openssh-server python3 shellcheck
 
 shellcheck osfiles/usr/local/sbin/sftpuseradd
 pip3 install flake8
@@ -10,7 +10,11 @@ flake8
 
 pip3 install -r requirements.txt
 
-sudo cp -p osfiles/etc/ssh/sshd_config /etc/ssh/sshd_config
+sudo cp -p osfiles/etc/nftables.conf /etc/
+sudo systemctl reload nftables 
+sudo nft list ruleset
+
+sudo cp -p osfiles/etc/ssh/sshd_config /etc/ssh/
 # systemctl enable sshd
 sudo systemctl reload sshd
 sudo cp -p osfiles/usr/local/sbin/sftpuseradd /usr/local/sbin/
