d42c9ee2d128e7fbac505c9671d11c551dbeaef7
Jeremy Busk <jeremybusk@gmail.com>
2020-03-11 07:23:34 +0000

    cause error

--- ci-test.sh
+++ ci-test.sh
@@ -2,6 +2,6 @@
 set -eo pipefail
 
 echo "test 1 file" > test1.txt
-echo "test 2 file" > test2.txt
+# echo "test 2 file" > test2.txt
 sftpuseradd --sftp-user jtest1 --sftp-pass jtest1
 ./sftp-test.py
--- install.sh
+++ install.sh
@@ -2,11 +2,9 @@
 set -exo pipefail
 
 sudo apt-get update
-sudo apt-get install -y python3
+sudo apt-get install -y openssh-server python3
 pip install -r requirements.txt
 
-sudo apt-get install -y openssh-server
-# apt-get install python3
 sudo cp -p osfiles/etc/ssh/sshd_config /etc/ssh/sshd_config
 # systemctl enable sshd
 sudo systemctl reload sshd
